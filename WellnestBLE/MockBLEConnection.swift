c53c86320dc92e73dfef35d1e92785069d7b4068
Dhruvi <dhruvi.p@wellnest.tech>
2023-04-06 18:38:40 +0530

    - Mock Bluetooth Serial change and framework created.

--- WellnestBLE/MockBLEConnection.swift
+++ WellnestBLE/MockBLEConnection.swift
@@ -12,8 +12,7 @@ import CoreBluetooth
 
 internal class MockBluetoothSerial: BluetoothSerial {
     
-    // MARK: Variables
-    
+    var latestFramework = "2.89"
     /// Whether this serial is ready to send and receive data
     override var isReady: Bool {
         get {
@@ -41,9 +40,11 @@ internal class MockBluetoothSerial: BluetoothSerial {
     
     /// Start scanning for peripherals
     override func startScan() {
-        let peripheral:WellnestPeripheral = WellnestPeripheral()
-        peripheral.name = "Wellnest ECG (Mock)"
-        delegate.serialDidDiscoverPeripheral(peripheral, RSSI: nil)
+        
+            let peripheral:WellnestPeripheral = WellnestPeripheral()
+            peripheral.name = "Wellnest ECG (Mock)"
+            delegate.serialDidDiscoverPeripheral(peripheral, RSSI: nil)
+        
     }
     
     /// Try to connect to the given peripheral
@@ -81,7 +82,20 @@ internal class MockBluetoothSerial: BluetoothSerial {
             self.delegate.serialDidReceiveString("+ELESTA=ff8");
         } else if (message.starts(with: "+GETSTA")) {
            self.delegate.serialDidReceiveString("+GETSTA=1,3");
-       }
+        } else if (message.starts(with: "+FWVERS")) {
+            self.delegate.serialDidReceiveString("Firmware Version : DazzleBLE-FW_\(latestFramework)")
+        }
+        else if (message.starts(with: "+OTAUPG")) {
+           self.delegate.serialDidDisconnect(connectedPeripheral!, error: nil)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 120, execute: {
+                var randomNumber = Int.random(in: 0...1)
+                if randomNumber == 0{
+                    self.latestFramework = "2.94"
+                    self.delegate.serialDidDiscoverPeripheral(self.connectedPeripheral!, RSSI: nil)
+                }
+            })
+        }
+            
     }
 }
 
